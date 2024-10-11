4ab130a475db3b9a6c3bad098bfa39865f6ae546
vr-greycube <66350441+vr-greycube@users.noreply.github.com>
2024-10-11 08:56:23 -0700

    add note for --no-mariadb-socket
    --no-mariadb-socket to create user

--- create-site.sh
+++ create-site.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# IMPORTANT: use --no-mariadb-socket so that db user is create as 'user'@'%' instead of e.g. 'user'@'x.x.x.x'
+
 bench new-site demo15.localhost --force --db-name demo15 --admin-password 123 \
 --db-root-username root --db-root-password 123 --no-mariadb-socket \
 # --install-app erpnext --install-app hrms --install-app payments --install-app india_compliance --install-app custom_app
